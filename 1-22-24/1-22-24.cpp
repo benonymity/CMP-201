babf57e97aa6a2240a4cc4152be51b5ec49aa57b
benonymity <benonymity@gmail.com>
2024-01-22 13:50:58 -0500

    day 3

---
+++ 1-22-24/1-22-24.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[] = {1, 2, 3, 4, 9, 50};
+
+    // // These three things do the same thing:
+    // cout << numbers[1] << endl;
+    // cout << *(numbers + 1) << endl;
+    // cout << 1[numbers] << endl;
+
+    cout << "Here's the modified list: ";
+
+    int length = sizeof(numbers)/sizeof(*numbers)-1;
+
+    for (int& num : numbers) {
+        if (num == numbers[length])
+            cout << num * 2;
+        else
+            cout << num * 2 << ", ";
+    }
+    cout << endl;
+
+    cout << "Here's the original list: ";
+
+    for (int num : numbers) {
+        if (num == numbers[length])
+            cout << num;
+        else
+            cout << num << ", ";
+    }
+    cout << endl;
+
+    // for (int i = 0; i < sizeof(numbers)/sizeof(*numbers); i += 1) 
+    //     cout << numbers[i] << endl;
+}
