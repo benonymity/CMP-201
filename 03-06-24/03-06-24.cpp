f10b705f7ad6c465c51e726acd8b45940b003710
benonymity <benonymity@gmail.com>
2024-03-06 13:51:51 -0500

    feat: class 21

---
+++ 03-06-24/03-06-24.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+
+class Item {
+  public:
+    Item()
+        : inventory_id(0), price(5) {
+        std::cout << "Item constructed\n";
+    }
+    Item(int i, int p)
+        : inventory_id(i), price(p) {
+        std::cout << "Item constructed\n";
+    }
+
+    int get_price() const {
+        return price;
+    }
+
+    std::string to_string() const {
+        std::string result;
+        result = "Price: " + std::to_string(price) +
+                 "\nInventory ID: " + std::to_string(inventory_id);
+        if (might_explode) {
+            result += "\nThis item might explode";
+        }
+        result += "\n";
+        return result;
+    }
+
+//   private:
+  protected: // Anything that's a subclass can access the data members
+    int price;
+    int inventory_id;
+    bool might_explode;
+};
+
+class Book : public Item {
+  public:
+    Book()
+        : title("none"),
+          isbn("none"),
+          author("Tony"),
+          pages(0),
+          inventory_id(0),
+          price(400) {
+        std::cout << "Book constructed\n";
+    }
+
+    void set_isbn(std::string isbn) {
+        this->isbn = isbn;
+    }
+
+    std::string get_isbn() const {
+        return isbn;
+    }
+
+    int get_price() const {
+        // return Item::get_price(); // if you don't want to use the protected keyword in the Item class
+        return Item::price;
+    }
+
+    std::string to_string() const {
+        std::string result;
+        result = "Price: $" + std::to_string(price) + "\nTitle: " + title +
+                 "\nAuthor: " + author + "\nPages: " + std::to_string(pages) +
+                 "\nISBN: " + isbn +
+                 "\nYear Published: " + std::to_string(publish_year) +
+                 "\nGenre: " + genre + "\nCourse: " + course +
+                 "\nInventory ID: " + std::to_string(inventory_id) +
+                 "\nText: " + text + "\n";
+        return result;
+    }
+
+  private:
+    int pages;
+    int price;
+    int publish_year;
+    int inventory_id;
+    std::string text;
+    std::string isbn;
+    std::string title;
+    std::string genre;
+    std::string author;
+    std::string course;
+};
+
+class Pen : public Item {
+  public:
+    Pen()
+        : width(0.0),
+          ink_color("fuscia"),
+          tip_type("ballpoint"),
+          might_explode(true),
+          manufacturer("---"),
+        //   price(5.0),
+        //   inventory_id(0),
+          Item(1, 10) {
+        std::cout << "Pen constructed\n";
+    }
+
+    std::string to_string() {
+        std::string result;
+        result = "Price: $" + std::to_string(price) +
+                 "\nWidth: " + std::to_string(width) +
+                 " mm\nInventory ID: " + std::to_string(inventory_id) +
+                 "\nManufacturer: " + manufacturer + "\n";
+        return result;
+    }
+
+    bool is_metal{true};
+  private:
+    // int price;
+    int width;
+    // int inventory_id;
+    bool might_explode;
+    std::string tip_type;
+    std::string ink_color;
+    std::string manufacturer;
+};
+
+int main() {
+    Book book;
+    std::cout << book.to_string() << "\n";
+
+    std::cout << book.get_price() << "\n";
+    std::cout << book.Item::get_price() << "\n\n";
+
+    Pen pen;
+    std::cout << pen.to_string() << "\n";
+    std::cout << pen.get_price() << "\n";
+    std::cout << pen.is_metal << "\n";
+
+    Item item;
+    item = book;
+    std::cout << item.to_string() << "\n";
+    std::cout << item.get_price() << "\n";
+    std::cout << item.is_metal << "\n";
+}
---
+++ 03-06-24/notes.md
@@ -0,0 +1,13 @@
+# CMP-201-01
+### Wednesday, March 6 - Class 21
+
+- New CS Professor!
+- Classes from now on:
+    - CMP-101: Introduction to Computer Science (2x each semester)
+    - CMP-201: Data Structures (each semester)
+    - CMP-202: Logic and Computing Structures (formal proofs, lots of fancy calc stuff. once a year for now, so this fall. maybe colisted as MTH-335)
+    - CMP- 310: Algorithims (once a year for now, so this fall. Taught by Serang)
+    - CMP-393: Philosophy of AI (this fall. has the Republic)
+    - MTH-425: Cryptography (in the math department that can count as CS)
+- `protected` keyword
+- 
